66ac24767792a4cbc14e10d45493fd8d958f4169
mecheddou nassim <166569147+mecheddou-nassim@users.noreply.github.com>
2024-08-12 00:30:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let image=document.querySelector(".a");
+let content=document.querySelector(".bdy");
+
+function changePhoto(phone){
+    image.src=phone;
+}
+function colors(color){
+  content.style.background=color;
+
+
+}
